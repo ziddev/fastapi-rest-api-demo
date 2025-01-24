12ea7b1c7fb951819b6db6e6c622aaad947611e7
ziddev <emmanuel.zidel-cauffet@ext.adeo.com>
2025-01-24 20:51:13 +0100

    chore: Nettoyage et renommage
    Signed-off-by: ziddev <emmanuel.zidel-cauffet@ext.adeo.com>

--- notes/src/main.py
+++
@@ -1,6 +0,0 @@
-from wsgi import app
-
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
--- notes/src/settings.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseSettings
-
-
-class Settings(BaseSettings):
-    DATA_DIRECTORY: str = "data"
-
-
-settings = Settings()
